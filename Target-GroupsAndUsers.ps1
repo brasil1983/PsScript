a3287cf5df5bc1bbaf3aaf971d48ccc53802731d
brasil1983 <brasil1983@live.it>
2017-10-15 11:42:58 +0200

    Add files via upload
    Create Hashtable to target users members of Security Groups

---
+++ Target-GroupsAndUsers.ps1
@@ -0,0 +1,21 @@
+﻿# create array to store users
+$users = @()
+
+# create array to store groups
+$groups = @(
+"itq it cinisello home","ITH it padova home"
+)
+
+# loop each group for members
+foreach ($group in $groups)
+{
+    $members = Get-ADGroupMember $group
+    foreach ($member in $members)
+    {
+        $user = Get-ADUser $member -Properties name, samaccountname, PasswordNeverExpires, PasswordExpired, Passwordlastset, UserPrincipalName
+        $users += $user
+    }
+}
+
+# output desired properties
+$users | select samaccountname, name , passwordexpired, passwordlastset,UserPrincipalName 
